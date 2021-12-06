38bc5ff50cbbf11355403347950267e8bd17d77f
iamericsu <89247843+iamericsu@users.noreply.github.com>
2021-12-06 21:30:38 +0800

    20211206_update_1

--- all.js
+++ all.js
@@ -1,15 +1,22 @@
 fetch("all.json").then(function(response){
     return response.json();
 }).then(function(resultData){
-    let ul = document.querySelector(".introduction");
-    for(let i = 0; i < resultData.length; i++){
-        let li = document.createElement("LI");
-        let header = document.createElement("H2");
-        header.innerHTML = resultData[i].header;
-        let content = document.createElement("P");
-        content.innerHTML = resultData[i].content;
-        li.appendChild(header);
-        li.appendChild(content);
-        ul.appendChild(li);
-    }
+    const ul = document.querySelector(".introduction");
+    let str = "";
+    resultData.forEach((item) => {
+        str += `<li><h3>${item.header}</h3>${item.content.map(render).join('')}</li>`;
+    });
+    ul.innerHTML = str;
 });
+
+function render(data) {
+    let str = "";
+    if(data.layout === undefined) { return str; };
+    
+    if(data.layout === "list") {
+        str = `<ul class="list">${data.content.map(item => `<li>${item}</li>`).join('')}</ul>`
+    } else {
+        str = data.content.map(item => `<p>${item}</p>`).join('');
+    }
+    return str;
+}
